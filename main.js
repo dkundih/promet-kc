656037393fd6ee6f9a211f44b3c6f5ef62f84eb9
David Kundih <kundihdavid@gmail.com>
2022-10-10 22:43:31 +0200

    changes

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-//data
-const api = "https://raw.githubusercontent.com/dkundih/promet-kc/master/geopodaci/T_vlak/T_vlak.json"
+import { m_bicikli_podaci } from "./pass_args.js"
 
-async function create(arg) {
-    const response = await fetch(api)
-    const data = await response.json()
-    return data[arg]
+//time
+
+function showTime() {
+    var time = new Date();
+    return time.toLocaleTimeString("hr-HR");
 }
-
-var bregi = await create("BREGI")
-var bregix = bregi['X']
-var bregiy = bregi['Y']
 
 //map
 
-const map = L.map('map').setView([45.8, 16], 12);
+const centermap = [[46.1639, 16.83], 14.8]
+
+const map = L.map('map').setView(centermap[0], centermap[1]);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -26,40 +24,20 @@ let locIcon = L.icon({
     iconAnchor:   [20, 30], // point of the icon which will correspond to marker's location
 })
 
-var markers = [L.marker([bregix, bregiy], {icon: locIcon}).addTo(map), L.marker([45.81, 16.14], {icon: locIcon}).addTo(map)]
+var mjesta = ["LENIŠĆE", "CERINE", "KAMPUS", "ŽELJEZNIČKI KOLODVOR", "GROBLJE", "ZRINSKI TRG", "DOM MLADIH"]
 
-function showTime() {
-    var time = new Date();
-    return time.toLocaleTimeString("hr-HR");
-}
-
-let first = true;
-
-
-function popup() {
-    for (let i of markers) {
-        if (first) {
-        i.bindPopup(
-        `
-        <H1>ZG</H1>
-        <p> ${showTime()} je sati ustani.</p>
-        <p>Hi</p>
-        <img src = "slike/vlak.jpg" height = 80px width = 200px</img>
-        `
-        ).openPopup();
-        }
-        else {
-            i.bindPopup(
-                `
-                <H1>ZG</H1>
-                <p> ${showTime()} je sati ustani.</p>
-                <p>Hi</p>
-                <img src = "slike/vlak.jpg" height = 80px width = 200px</img>
-                `
-                )
-        }
+async function prikazMBicikala() {
+    for (let i of mjesta) {
+        var j = await L.marker([m_bicikli_podaci[i]['X'], m_bicikli_podaci[i]['Y']], {icon: locIcon}).addTo(map)
+        j.bindPopup(
+            `
+            <H4>${i}</H4>
+            <p>Sada je ${showTime()}.</p>
+            <p>Ovdje se nalazi postolje mehaničkih <b>Bicko</b> bicikala.</p>
+            <img src = "slike/bicikli.jpg" height = 150px width = 300px</img>
+            `
+        )
     }
-    first = false
 }
 
-setInterval(popup, 1000)
+setInterval(prikazMBicikala, 1000)
